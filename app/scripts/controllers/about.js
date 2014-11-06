01f4d1f3fdfbf7c7bd1c04812b109b3c220873a1
Abdullah Alger <abdullahalger@me.com>
2014-11-06 15:35:50 +0200

    first commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name evervoiceApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the evervoiceApp
+ */
+angular.module('evervoiceApp')
+  .controller('AboutCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name evervoiceApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the evervoiceApp
+ */
+angular.module('evervoiceApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
