a896433d683d7fd8cce52c4f2b5e85ad5b4344ae
Abdullah Alger <abdullahalger@me.com>
2014-11-08 22:30:57 +0200

    Messing around with directives and speech api

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -5,15 +5,30 @@ var evervoice = angular
     'ngSanitize'
   ]);
 
-evervoice.controller('MainCtrl', ['$scope', function($scope) {
+//evervoice.controller('CommandCtrl', ['$scope', function($scope) {
 
-  var commands = {
-      'new note': function() {
+  //var commands = {
+    //  'new note': function() {
+      //  console.log('new note activated');
+     // },
+    //  'save note': function() {
+      //  console.log('save note activated');
+     // },
+   //   'what *term do you like': function() {
+    //    alert('Whatever you like!');
+   //   }
+  //};
 
-      }
+  //annyang.addCommands(commands);
+  //annyang.start();
+
+//}]);
+
+evervoice.directive('recordVoice', function() {
+  return {
+    restrict: 'E',
+    controller: function($scope) {
+      console.log('firing directive');
+    }
   };
-
-  annyang.addCommands(commands);
-  annyang.start();
-
-}]);
+});
