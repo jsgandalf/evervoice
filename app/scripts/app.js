bfae8da7962019922730da38370fdfd60de7bc0b
Abdullah Alger <abdullahalger@me.com>
2014-11-10 21:58:26 +0200

    Speech API working - Working on voice commands and Evernote API

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -8,53 +8,34 @@ var evervoice = angular
 
 evervoice.controller('myCtrl', function($scope) {
 
-  var commands = {
-
-    'new note': function() {
-      $scope.startRecognition();
-      console.log('new note activated');
-    },
-    'save note': function() {
-      console.log('save note activated');
-    }
-  };
-
-  annyang.addCommands(commands);
-  annyang.start();
-
 
   $scope.startRecognition = function(){
 
-      var recognition = new webkitSpeechRecognition();
-      $scope.recognizing = true;
-      recognition.lang = ['English', ['en-US', 'United States']];
-      recognition.continuous = true;
-      recognition.interimResults = true;
+    var recognition = new webkitSpeechRecognition();
+    $scope.recognizing = true;
+    recognition.lang = ['English', ['en-US', 'United States']];
+    recognition.continuous = true;
+    recognition.interimResults = true;
 
-      recognition.onstart = function(e){
-        console.log('Recognizing speech...');
-      };
-
-      recognition.onspeechend = function(e){
-        console.log('Speech processed.');
-        $scope.recognizing = false;
-      };
-
-      recognition.onresult = function(e){
-        var interimTranscript = '';
-        if (e.results.length) {
-          for (var i = e.resultIndex; i < e.results.length; i++) {
-            interimTranscript = e.results[i][0].transcript;
-            console.log('Transcript: ', interimTranscript);
-          }
-        }
-
-        $scope.interimTranscript;
-
-      };
-      recognition.start();
-      console.log('firing start');
+    recognition.onstart = function(e){
+      console.log('Recognizing speech...');
     };
+
+    recognition.onspeechend = function(e){
+      console.log('Speech processed.');
+      $scope.recognizing = false;
+    };
+
+    recognition.onresult = function(e){
+      var interimTranscript = '';
+      for (var i = e.resultIndex; i < e.results.length; i++) {
+        $scope.interimTranscript = e.results[i][0].transcript;
+        console.log('Transcript: ', $scope.interimTranscript);
+      }
+      $scope.$apply(interimTranscript);
+    };
+    recognition.start();
+  }
 
 });
 
